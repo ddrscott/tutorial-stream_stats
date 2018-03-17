e15502b23c1428f5ff86fdf6c6d791221e456992
Scott Pierce <scott.pierce@centro.net>
2018-03-17 00:46:13 -0500

    Step 4 - Write to `/dev/tty`
    Swap out the `eprintln!` with `/dev/tty` and give `Stats` struct a
    constructor.

--- stream_stats.rs
+++ stream_stats.rs
@@ -1,3 +1,4 @@
+use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::time::Instant;
 
@@ -6,17 +7,28 @@ static READ_BUF_SIZE: usize = 1024 * 1024;
 struct Stats {
     started: Instant,
     lines: usize,
+    tty: File,
+}
+
+impl Stats {
+    fn new(tty: &str) -> Stats {
+        Stats {
+            started: Instant::now(),
+            lines: 0,
+            tty: OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(tty)
+                .expect("Cannot open tty for writing!"),
+        }
+    }
 }
 
 fn main() {
     let mut reader = BufReader::with_capacity(READ_BUF_SIZE, io::stdin());
     let mut writer = BufWriter::new(io::stdout());
     let mut buffer = vec![];
-
-    let mut stats = Stats {
-        started: Instant::now(),
-        lines: 0,
-    };
+    let mut stats = Stats::new("/dev/tty");
 
     while reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
         writer.write(&buffer).unwrap();
@@ -24,5 +36,10 @@ fn main() {
         buffer.clear();
     }
     writer.flush().unwrap();
-    eprintln!("lines: {}, {:?}", stats.lines, stats.started.elapsed());
+    writeln!(
+        stats.tty,
+        "lines: {}, {:?}",
+        stats.lines,
+        stats.started.elapsed()
+    ).expect("Could not write to tty!");
 }
