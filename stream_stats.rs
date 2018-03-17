30da32426f7ac420f4660c168678341301c68648
Scott Pierce <scott.pierce@centro.net>
2018-03-17 00:38:45 -0500

    Step 2 - Add buffering
    Buffering gets us performance closer to native `cat` command.

--- stream_stats.rs
+++ stream_stats.rs
@@ -1,9 +1,15 @@
-use std::io;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+
+static READ_BUF_SIZE: usize = 1024 * 1024;
 
 fn main() {
-    let mut buffer = String::new();
-    while io::stdin().read_line(&mut buffer).unwrap() > 0 {
-        print!("{}", buffer);
+    let mut reader = BufReader::with_capacity(READ_BUF_SIZE, io::stdin());
+    let mut writer = BufWriter::new(io::stdout());
+    let mut buffer = vec![];
+
+    while reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
+        writer.write(&buffer).unwrap();
         buffer.clear();
     }
+    writer.flush().unwrap();
 }
