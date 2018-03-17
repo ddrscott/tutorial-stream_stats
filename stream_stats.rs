4131ec8daea852ec4641cbca9ba13775ff8679d5
Scott Pierce <scott.pierce@centro.net>
2018-03-17 00:40:56 -0500

    Step 3 - Count the lines, run time, and print them
    We introduce a `struct` to hold the statistics and increment them along
    the way. After we're done streaming the data, we output the stats to stderr.
    It's not ideal, but we'll fix that in the next step.

--- stream_stats.rs
+++ stream_stats.rs
@@ -1,15 +1,28 @@
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::time::Instant;
 
 static READ_BUF_SIZE: usize = 1024 * 1024;
+
+struct Stats {
+    started: Instant,
+    lines: usize,
+}
 
 fn main() {
     let mut reader = BufReader::with_capacity(READ_BUF_SIZE, io::stdin());
     let mut writer = BufWriter::new(io::stdout());
     let mut buffer = vec![];
 
+    let mut stats = Stats {
+        started: Instant::now(),
+        lines: 0,
+    };
+
     while reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
         writer.write(&buffer).unwrap();
+        stats.lines += 1;
         buffer.clear();
     }
     writer.flush().unwrap();
+    eprintln!("lines: {}, {:?}", stats.lines, stats.started.elapsed());
 }
