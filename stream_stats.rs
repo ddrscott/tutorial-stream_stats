d2c5ef1cfcffc6a54e4b669aae835051d262143d
Scott Pierce <scott.pierce@centro.net>
2018-03-17 00:51:52 -0500

    Step 5 - Nicely format stats output
    We implemant a `fmt::Display` trait to own the final representation of
    the Stats struct and add kilobytes processed.

--- stream_stats.rs
+++ stream_stats.rs
@@ -1,12 +1,15 @@
+use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::time::Instant;
 
 static READ_BUF_SIZE: usize = 1024 * 1024;
+static CLEAR_LINE: &str = "\x1B[1G\x1B[2K";
 
 struct Stats {
     started: Instant,
     lines: usize,
+    bytes: usize,
     tty: File,
 }
 
@@ -15,12 +18,37 @@ impl Stats {
         Stats {
             started: Instant::now(),
             lines: 0,
+            bytes: 0,
             tty: OpenOptions::new()
                 .write(true)
                 .append(true)
                 .open(tty)
                 .expect("Cannot open tty for writing!"),
         }
+    }
+}
+
+impl fmt::Display for Stats {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+        let elapsed = self.started.elapsed();
+        let seconds: f64 = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
+        if seconds == 0.0 {
+            return write!(f, "");
+        }
+        let kb = self.bytes as f64 / 1024 as f64;
+        let kb_per_sec = kb / seconds;
+        let lines_per_sec = self.lines as f64 / seconds;
+        write!(
+            f,
+            "{}{:.1} sec | {:.0} kb [ {:.1}/s ] | {} lines [ {:.0}/s ]",
+            CLEAR_LINE,
+            seconds,
+            kb,
+            kb_per_sec,
+            self.lines,
+            lines_per_sec
+        )
     }
 }
 
@@ -33,13 +61,9 @@ fn main() {
     while reader.read_until(b'\n', &mut buffer).unwrap() > 0 {
         writer.write(&buffer).unwrap();
         stats.lines += 1;
+        stats.bytes += &buffer.len();
         buffer.clear();
     }
     writer.flush().unwrap();
-    writeln!(
-        stats.tty,
-        "lines: {}, {:?}",
-        stats.lines,
-        stats.started.elapsed()
-    ).expect("Could not write to tty!");
+    writeln!(&stats.tty, "{}", &stats).expect("Could not write to tty!");
 }
